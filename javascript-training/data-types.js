3301c11550ae93f9719ae7c4539a62fb10869808
jagdalemanoj7 <jagdalemanoj7@gmail.com>
2026-06-27 15:43:10 +0530

    Assignment 1

---
+++ javascript-training/data-types.js
@@ -0,0 +1,6 @@
+let sal='20000'
+console.log(sal)
+sal=null
+console.log(sal)
+let age;
+console.log(age)
---
+++ javascript-training/hello-world.js
@@ -0,0 +1,8 @@
+console.log('Hello World..!!')
+
+console.info('info msg')
+console.warn('warning msg')
+console.error('error msg')
+/**
+ * 
+ */
