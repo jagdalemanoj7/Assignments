785f62c0651d2bab822f6952f0d13d48cd3ed855
jagdalemanoj7 <jagdalemanoj7@gmail.com>
2026-06-30 18:36:54 +0530

    first assignment

---
+++ typescript-training/Assignments/Assignment1.ts
@@ -0,0 +1,104 @@
+console.log(`1. Temperature of a city in degrees Celsius: 25.5`) 
+let tempreture:number=25.5
+console.log(`Tempreture of City: ${tempreture}`)
+console.log(`----------------------------------------------------------`)
+
+console.log(`2. Whether a customer has placed an order: true or false`) 
+let orderStatus:boolean = true
+console.log(`Order Status: ${orderStatus}`)
+console.log(`----------------------------------------------------------`)
+
+console.log(`3. Person's phone number: "123-456-7890"`) 
+let perContact: string = "123-456-7890"
+console.log(`Person's Phone Number:  ${perContact}`)
+console.log(`----------------------------------------------------------`)
+
+console.log(`4. Amount of money in a customer's bank account: 1000.50`) 
+let saving:number=1000.50
+console.log(`Saving Amount: ${saving}`)
+console.log(`----------------------------------------------------------`)
+
+console.log(`5. Person's email address: "john.doe@example.com"`) 
+let email:string="john.doe@example.com"
+console.log(`Email Address: ${email}`)
+console.log(`----------------------------------------------------------`)
+
+console.log(`6. Coordinates of a location (latitude, longitude): 37.7749, -122.4194`) 
+let coordLocation:number[]=[37.7749, -122.4194]
+console.log(`Latitude: ${coordLocation[0]}, Longitude: ${coordLocation[1]}`)
+//                              OR
+interface Coordinates{
+    latitude:number,
+    longitude:number
+}
+let coordinates:Coordinates={latitude:37.7749, longitude:-122.4194}
+console.log(`Coordinates: ${coordinates.latitude}, ${coordinates.longitude}`)
+console.log(`----------------------------------------------------------`)
+
+console.log(`7. Person's marital status: true or false`) 
+let maritalStatus : boolean = true
+console.log(`Person's Marital Status: ${maritalStatus}`)
+console.log(`----------------------------------------------------------`)
+
+console.log(`8. Person's occupation: "Software Engineer"`) 
+let personOccupation: string = "Software Engineer"
+console.log(`Person's Occupation : ${personOccupation}`)
+
+console.log(`9. Person's favourite colour: "Blue"`) 
+let colour:string="Blue"
+console.log(`Person's favourite Colour: ${colour}`)
+console.log(`----------------------------------------------------------`)
+
+console.log(`10.Current year: 2023`) 
+let currentDate:number=2023
+console.log(`Current Date: ${currentDate}`)
+console.log(`----------------------------------------------------------`)
+
+console.log(`11.Number of followers on a social media platform: 1,000,000`) 
+let followers:string='1,000,000'
+console.log(`Number of followers on a social media platform: ${followers}`)
+console.log(`----------------------------------------------------------`)
+
+console.log(`12.Rating of a movie: 7.5`) 
+let rating:number=7.5
+console.log(`Rating of a movie: ${rating}`)
+console.log(`----------------------------------------------------------`)
+
+console.log(`13.Person's blood type: 'A'`) 
+let bloodtype:string = 'A'
+console.log(`Person's blood type: ${bloodtype}`)
+console.log(`----------------------------------------------------------`)
+
+console.log(`14.Title of a book: "To Kill a Mockingbird"`) 
+let title:string='To kill a Mockingbird'
+console.log(`Title of a book: ${title}`)
+console.log(`----------------------------------------------------------`)
+
+console.log(`15.Number of employees in a company: 500`) 
+let numberOfEmployee:number=500
+console.log(`Number of emplyees in a company: ${numberOfEmployee}`)
+console.log(`----------------------------------------------------------`)
+
+console.log(`16.Time of an event: 2:30 PM`) 
+let time:string ='2:30 PM'
+console.log(`Time of an event: ${time}`)
+console.log(`----------------------------------------------------------`)
+
+console.log(`17.Name of a country: "United States"`) 
+let nameOfCountry: string='United States'
+console.log(`Name of a country: ${nameOfCountry}`)
+console.log(`----------------------------------------------------------`)
+
+console.log(`18.Person's eye color: "Brown"`) 
+let eyeColor: string = 'Brown'
+console.log(`Person's eye color: ${eyeColor}`)
+console.log(`----------------------------------------------------------`)
+
+console.log(`19.Person's birthplace: "New York City"`) 
+let birthPlace:string = 'New York City'
+console.log(`Person's birthplace: ${birthPlace}`)
+console.log(`----------------------------------------------------------`)
+
+console.log(`20. Distance between two cities: 200.5`) 
+let distance: number=200.5
+console.log(`Distance between two cities: ${distance}`)
